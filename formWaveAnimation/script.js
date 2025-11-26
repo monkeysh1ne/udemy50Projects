399d3a2bac1b6d90d1b6a16b427cb36ef369bc79
monkeysh1ne <mcdoob@gmail.com>
2025-11-26 13:40:21 +1000

    animte input labels formWaveAnimation

--- formWaveAnimation/script.js
+++ formWaveAnimation/script.js
@@ -1,8 +1,13 @@
+/* create node list of all the labels */
 const labels = document.querySelectorAll('.form-control label')
 
+/* iterate through the letters of the label and surround each letter with  <span> element */
 labels.forEach((label) => {
-  label.innerHTML = label.innerHTML
+  label.innerHTML = label.innerText
     .split('')
-    .map((letter, idx) => `<span>${letter}</span>`)
+    .map(
+      (letter, idx) =>
+        `<span style="transition-delay: ${idx * 50}ms">${letter}</span>`
+    )
     .join('')
 })
